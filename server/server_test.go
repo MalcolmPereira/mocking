2ac9d62f613ea87710f1f5988a1728d6f8e44aa3
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2021-02-23 18:46:39 -0500

    updates to write mock for image api

--- server/server_test.go
+++ server/server_test.go
@@ -8,7 +8,7 @@ import (
 
 //TestValidateRequired tests for required input
 func TestProcessYAML(t *testing.T) {
-	mockingYAML, err := processMockingYAML("../mocking.yaml")
+	mockingYAML, err := ProcessMockingYAML("../mocking.yaml")
 	if err != nil {
 		t.Fatalf("FAIL TestProcessYAML test for valid input")
 	}
